d323ca8b3c1b71033f8ca3d0f7cc0c1e13491d69
eaglikpo-lab <eaglikpo@gmail.com>
2025-09-03 17:55:08 +0100

    Correction de l'exercice etudiant

--- student.php
+++ student.php
@@ -13,25 +13,12 @@ class Student {
     }
         
     // TODO: Eviter les codes morts. Supprimer si plus besoin
-    /*public function addGrade (float $grade): void {
-        $new_note = 0;
-        if ($grade <0 && $grade>20) {
-            do {
-                echo "Invalid grade. Put a right grade here: \n";
-                $new_note = (float)trim(fgets(STDIN));
-            }while ($new_note>=0 && $new_note<=20);  
-            $grade = $new_note;
-        }
-          $this->grades[] = $grade; 
-    }*/
 
     public function addGrade (float $grade): void {
-        $new_note = 0; // TODO: inutilisé donc inutile
         if ($grade >=0 && $grade<=20) {
             $this->grades[] = $grade;
         }else {
             echo "Invalid grade. \n";
-            //$new_note = (float)trim(fgets(STDIN)); // TODO: Code mort, supprimer si pas besoin 
         }
     }
     
@@ -39,28 +26,32 @@ class Student {
         // TODO: Pourrait être simplifié. Check "array_splice()"
         foreach ($this->grades as $key =>$g) {
             if ($grade == $g) {
-                unset($this->grades[$key]); 
+                array_splice($this->grades, $key, 1);
             }
-            $this->grades = array_values($this->grades);
         }
     }
 
     public function average() : float {
         $somme = 0;
-        foreach ($this->grades as $key =>$g) {
-            $somme += $g; 
-        }
         echo "Your average is: ";
-        // TODO: correct mais division par 0 possible. Gérer ce cas. Juste retourner 0 si pas de notes
-        $this->moyenne = $somme/count($this->grades); 
-        return $this->moyenne;
+        if (count($this->grades)!=0) {
+            foreach ($this->grades as $key =>$g) {
+                $somme += $g; 
+            }
+            // TODO: correct mais division par 0 possible. Gérer ce cas. Juste retourner 0 si pas de notes
+            $this->moyenne = $somme/count($this->grades); 
+            return $this->moyenne;
+        } else {
+            return $this->moyenne = 0;
+        }
+        
     }
 
     public function honor() : void{
         // TODO: Good
+        echo "Honor: ";
         if ($this->moyenne <10) {
             echo "Insuffisant\n" ;
-            echo $this->moyenne;
         } elseif ($this->moyenne>=10 && $this->moyenne<12) {
             echo "Passable\n";
         } elseif ($this->moyenne>=12 && $this->moyenne<14) {
